dc9e0188223f7d41f6ecc2fcaa7a3ddeb237168f
Jimin Wen <jiminwen@tamu.edu>
2017-10-05 05:06:25 +0000

    new and select

--- movie_app/echo.js
+++ movie_app/echo.js
@@ -160,16 +160,42 @@ app.get("/actors", function(req, res) {
     var order_item = "actor.id";
     var order = "asc";
     var page = { number: cur_number, items_per_page: cur_items, order_item: order_item, order: order };
+    var selected = ["actor.id", "actor.actor_name", "actor.facebook_likes", "group_concat(movie.title order by movie.id) as movies", "group_concat(director.director_name order by director.id) as directors"];
+    var new_select = [];
+    var select_string = "";
     console.log(req.query);
     if (!empty(req.query)) {
-        order = req.query.order;
-        order_item = req.query.order_item;
-        page.order = order;
-        page.order_item = order_item;
+        if (req.query.order != undefined) {
+            order = req.query.order;
+            page.order = order;
+        }
+        if (req.query.order_item != undefined) {
+            order_item = req.query.order_item;
+            page.order_item = order_item;
+        }
+        if (req.query.number == "chosen") {
+            new_select.push("actor.id");
+        }
+        if (req.query.name == "chosen") {
+            new_select.push("actor.actor_name");
+        }
+        if (req.query.facebook_likes == "chosen") {
+            new_select.push("actor.facebook_likes");
+        }
+        if (req.query.movie_attended == "chosen") {
+            new_select.push("group_concat(movie.title order by movie.id) as movies");
+        }
+        if (req.query.director == "chosen") {
+            new_select.push("group_concat(director.director_name order by director.id) as directors");
+        }
     }
-    var sql = "select actor.id, actor.actor_name, actor.facebook_likes, " +
-        "group_concat(movie.title order by movie.id) as movies, " +
-        "group_concat(director.director_name order by director.id) as directors from actor " +
+    console.log(new_select);
+    if (!empty(new_select)) {
+        selected = new_select.slice();
+    }
+    select_string = selected.join();
+    console.log(select_string);
+    var sql = "select " + select_string + " from actor " +
         "inner join movie_actor_relation on actor.id=movie_actor_relation.actor_id " +
         "inner join movie on movie_actor_relation.movie_id = movie.id " +
         "inner join director on movie.director_id=director.id group by actor.id order by " + order_item + " " + order;
@@ -188,15 +214,43 @@ app.get("/directors", function(req, res) {
     var order_item = "director.id";
     var order = "asc";
     var page = { number: cur_number, items_per_page: cur_items, order_item: order_item, order: order };
+    var selected = ["director.id", "director.director_name", "director.directo_facebook_likes", "group_concat(movie.title order by movie.id) as movies", "group_concat(actor.actor_name order by actor.id) as actors"];
+    var new_select = [];
+    var select_string = "";
+    console.log(req.query);
     if (!empty(req.query)) {
-        order = req.query.order;
-        order_item = req.query.order_item;
-        page.order = order;
-        page.order_item = order_item;
+        if (req.query.order != undefined) {
+            order = req.query.order;
+            page.order = order;
+        }
+        if (req.query.order_item != undefined) {
+            order_item = req.query.order_item;
+            page.order_item = order_item;
+        }
+        if (req.query.number == "chosen") {
+            new_select.push("director.id");
+        }
+        if (req.query.name == "chosen") {
+            new_select.push("director.director_name");
+        }
+        if (req.query.facebook_likes == "chosen") {
+            new_select.push("director.directo_facebook_likes");
+        }
+        if (req.query.movies == "chosen") {
+            new_select.push("group_concat(movie.title order by movie.id) as movies");
+        }
+        if (req.query.actors == "chosen") {
+            new_select.push("group_concat(actor.actor_name order by actor.id) as actors");
+        }
     }
-    var sql = "select director.id, director.director_name, director.directo_facebook_likes, group_concat(movie.title order by movie.id) as movies," +
-        " group_concat(actor.actor_name order by actor.id) as actors " +
-        "from director inner join movie on director.id=movie.director_id " +
+    console.log(new_select);
+    if (!empty(new_select)) {
+        selected = new_select.slice();
+    }
+    select_string = selected.join();
+    console.log(select_string);
+    var sql = "select " + select_string +
+        " from director inner join movie on director.id=movie.director_id " +
         "inner join movie_actor_relation on movie.id=movie_actor_relation.movie_id " +
         "inner join actor on movie_actor_relation.actor_id=actor.id " +
         "group by director.id order by " + order_item + " " + order;
